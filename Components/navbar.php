3a7f52433306fb6d93b4da9acb37e112a1ccf379
flocane <locane.francisco@gmail.com>
2019-08-29 20:40:51 -0300

    dividiendo en componentes

---
+++ Components/navbar.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg navbar-light ">
+<a class="navbar-brand" href="#">Lavadero Lava Bien</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNav">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Productos</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="formulario.php">Contacto</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#" >Sobre Nosotros</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="registro.php" >Registrate</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="login.php" >Ingresa</a>
+    </li>
+  </ul>
+</div>
+</nav>
