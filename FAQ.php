740bd206e9448bdcdd5dbf31562a35c3ba22a119
Gise <sanchezgisela85@gmail.com>
2019-09-08 13:00:05 -0300

    faq y css del faq

---
+++ FAQ.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+    <title>e-commerce</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.1.1/bootstrap-social.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.1.1/bootstrap-social.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.1.1/bootstrap-social.min.css.map">
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Righteous&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/GS-style.css">
+</head>
+<body >
+<div class="container-fluid px-0">
+<?php  include_once 'Components/navbar.php' ?>
+
+<nav class="gral">
+ <main>
+    <section id="titulo  faq">
+        <p class="PregFAQTitutlo" > <strong> Preguntas Frecuentes</strong></p>
+    </section>
+     
+    <section class="seccionx">
+        <p class="tituloseccion"> <strong>Sobre el lavado</strong></p>
+          
+          <article>
+          <p class="pregunta">¿Cuál es la diferencia entre bolsa de lavandería, plancha y tintorería?</p>
+          <p class="rta"> En nuestras Sucursales Jeff desarrollamos tres procesos: 
+            Lavandería que incluye lavado en agua, secado en secarropas y doblado.
+            Plancha que incluye lavado en agua, secado en secarropas y planchado.
+            Tintorería que incluye desmanchado, limpieza en seco, lavado y planchado.</p>
+         </article>
+    
+         <article>
+          <p class="pregunta">¿Cómo puedo saber qué prendas deben ser lavadas en agua y cuáles en tintorería?</p>
+          <p class="rta"> Por lo general, las prendas lavadas en agua son todas aquellas que normalmente lavás en el lavarropas. 
+          Las prendas de tintorería son aquellas que  requieren de un tratado especial ya sea para prendas más delicadas o aquellas que tengan manchas más profundas.</p>
+         </article>
+
+         <article>
+          <p class="pregunta">¿Lavan materiales especiales?</p>
+          <p class="rta"> ¡Por supuesto! Podés consultarlo en tu Sucursal Jeff más cercana.</p>
+         </article>
+
+         <article>
+          <p class="pregunta">¿Hay algún producto que no laven? </p>
+          <p class="rta">Por el momento no lavamos zapatillas ni bolsos. Si querés informarte de algún producto en concreto que no aparezca en nuestra web podés escribir a info@lavaderolavabien.com.</p>
+         </article>
+    </section>
+
+    <section class="seccionx">
+        <p class="tituloseccion"> <strong>Hacer el pedido</strong></p>
+          
+          <article>
+          <p class="pregunta">¿Qué debo hacer para realizar un pedido?</p>
+          <p class="rta">Podés realizar tu pedido desde la web.</p>
+         </article>
+    
+         <article>
+          <p class="pregunta">¿Tengo que registrarme para realizar mi primer pedido?</p>
+          <p class="rta"> Si nunca has hecho un pedido puedes registrarte con Facebook, google o email.</p>
+         </article>
+    </section>
+
+    <section class="seccionx">
+        <p class="tituloseccion"> <strong>Pago</strong></p>
+          
+          <article>
+          <p class="pregunta">¿Cómo hago el pago?</p>
+          <p class="rta">Aceptamos tarjetas de Visa/MasterCard/American Express, Paypal y pago en efectivo.</p>
+         </article>
+    
+         <article>
+          <p class="pregunta">¿Qué métodos de pago están autorizados?</p>
+          <p class="rta"> Si nunca has hecho un pedido puedes registrarte con Facebook, google o email.</p>
+         </article>
+    </section>
+
+    <section class="seccionx">
+        <p class="tituloseccion"> <strong>Atención al cliente</strong></p>
+          
+          <article>
+          <p class="pregunta">¿Qué pasa si mis prendas necesitan un tratamiento diferente?</p>
+          <p class="rta">En ese caso, te contactaremos para que autorices cualquier tratamiento de las prendas antes de realizarlo.</p>
+         </article>
+    
+         <article>
+          <p class="pregunta">¿Qué ocurre si mis prendas se dañan?</p>
+          <p class="rta">Tus prendas están en las mejores manos y los incidentes son algo extremadamente raro, aunque entendemos tu preocupación. Por ello estamos asegurados.</p>
+         </article>
+    </section>
+ 
+ </main>
+
+
+</nav>
+
+<!-- color #94d3f8-->
+
+
+
+</body>
+</html>
+<?php include_once 'Components/footer.php'; ?>
