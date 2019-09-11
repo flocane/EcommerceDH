159766013f4e45ff1ad505e2fca99432096a6469
Unknown <sanchezgisela85@gmail.com>
2019-09-10 21:25:21 -0300

    Ajustes en prductPage

--- productPage.php
+++ productPage.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Righteous&display=swap" rel="stylesheet">
+    
 </head>
 <body >
 <div class="container-fluid px-0">
@@ -27,32 +28,56 @@
 
         <div class="producto">      
             <img src="img/lavadora.png" alt="Lavado Small" style="width:100%; ">
-            <p> Lavado Small </p>
+            <h4 class="nombreProd">Lavado Small</h4>
+              <p class="precio">Precio: <em class="precio">$449</em> </p>
+              <div> 
+                <button class="botonSubmit">Agregar</button>
+              </div>
         </div>
 
         <div class="producto">      
             <img src="img/lavadora.png" alt="Lavado Medium" style="width:100%;">
-            <p> Lavado Medium </p>
+            <h4 class="nombreProd">Lavado Medium</h4>
+              <p class="precio">Precio: <em class="precio">$549</em> </p>
+              <div> 
+                <button class="botonSubmit">Agregar</button>
+              </div>
         </div>
 
         <div class="producto">      
             <img src="img/lavadora.png" alt="Lavado large" style="width:100%;">
-            <p> Lavado Large </p>
+            <h4 class="nombreProd">Lavado Large</h4>
+              <p class="precio">Precio: <em class="precio">$649</em> </p>
+              <div> 
+                <button class="botonSubmit">Agregar</button>
+              </div>
         </div>
 
         <div class="producto">      
             <img src="img/planchado2.png" alt="Planchado" style="width:100%;">
-            <p> Planchado </p>
+            <h4 class="nombreProd">Planchado</h4>
+              <p class="precio">Precio: <em class="precio">$415</em> </p>
+              <div> 
+                <button class="botonSubmit">Agregar</button>
+              </div>
         </div>
 
         <div class="producto">      
             <img src="img/secado.png" alt="Secado" style="width:100%;">
-            <p> Secado </p>
+            <h4 class="nombreProd">Secado</h4>
+              <p class="precio">Precio: <em class="precio">$230</em> </p>
+              <div> 
+                <button class="botonSubmit">Agregar</button>
+              </div>
         </div>
 
         <div class="producto">      
             <img src="img/lavado-en-seco.png" alt="Limpieza" style="width:100%;">
-            <p> Limpieza </p>
+            <h4 class="nombreProd">Limpieza</h4>
+              <p class="precio">Precio: <em class="precio">$346</em> </p>
+              <div> 
+                <button class="botonSubmit">Agregar</button>
+              </div>
         </div>
 
     </div>
