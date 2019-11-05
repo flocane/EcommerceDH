069fd092b08ccb7ac1286a4b0827c02bc245a189
flocane <locane.francisco@gmail.com>
2019-11-05 12:28:14 -0300

    conexion PDO

---
+++ Classes/prueba.php
@@ -0,0 +1,73 @@
+<?php
+class Persona {
+    private $nombre;
+    private $apellido;
+    private $email;
+
+    public function __construct($nombre,$apellido,$email){
+        $this->nombre=$nombre;
+        $this->apellido=$apellido;
+        $this->email=$email;
+
+    }
+
+    /**
+     * Get the value of nombre
+     */ 
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @return  self
+     */ 
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of apellido
+     */ 
+    public function getApellido()
+    {
+        return $this->apellido;
+    }
+
+    /**
+     * Set the value of apellido
+     *
+     * @return  self
+     */ 
+    public function setApellido($apellido)
+    {
+        $this->apellido = $apellido;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}    
